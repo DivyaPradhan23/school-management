b0d0ea47bdc53824fbe667ca044e6ca2ac826f6b
Divya Pradhan <149256006+DivyaPradhan23@users.noreply.github.com>
2025-09-01 13:03:06 +0530

    Update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,5 +1,18 @@
 import mysql from "mysql2/promise";
 
-const connection = mysql.createPool(process.env.DATABASE_URL);
+// parse DATABASE_URL from environment
+const { DATABASE_URL } = process.env;
+const url = new URL(DATABASE_URL);
+
+const connection = mysql.createPool({
+  host: url.hostname,
+  user: url.username,
+  password: url.password,
+  database: url.pathname.slice(1), // remove the leading '/'
+  port: url.port,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
 export default connection;
