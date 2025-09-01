1cc014df488963bf25e81aa95d4a0776265290ca
Divya Pradhan <149256006+DivyaPradhan23@users.noreply.github.com>
2025-09-01 16:00:55 -0400

    Update route.js

--- src/app/api/schools/route.js
+++ src/app/api/schools/route.js
@@ -14,12 +14,12 @@ export async function POST(req) {
     const email_id = formData.get("email_id");
     const file = formData.get("file");
 
-    // Save image in public/schoolImages
+    // Save image in public/
     const bytes = await file.arrayBuffer();
     const buffer = Buffer.from(bytes);
     const filename = Date.now() + "-" + file.name;
 
-    const uploadDir = path.join(process.cwd(), "public", "schoolImages");
+    const uploadDir = path.join(process.cwd(), "public", "school");
     await fs.mkdir(uploadDir, { recursive: true });
     await fs.writeFile(path.join(uploadDir, filename), buffer);
 
